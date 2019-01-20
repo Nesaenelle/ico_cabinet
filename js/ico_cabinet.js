8e242a1cc136937bf848a0a88e14846172e5182e
Bohdan Zinkevych <venomsunset@gmail.com>
2019-01-20 21:26:14 +0200

    fix

--- js/ico_cabinet.js
+++ js/ico_cabinet.js
@@ -1,3 +1,28 @@
+;
+(function () {
+
+    // var self = this;
+    var clock = $('[data-clock]');
+    var dateEl = clock.find('[data-clock-date]');
+    var time = clock.find('[data-clock-time]');
+    // var self = this;
+
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    update();
+    setInterval(function () {
+        update();
+    }, 1000);
+
+    function update() {
+        var date = new Date();
+        dateEl[0].innerHTML = monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+        time[0].innerHTML = date.toTimeString().split(' ')[0];
+    }
+
+}());
+
 (function ($) {
     var tabs = $("[data-tabs-item]");
     var contents = $("[data-tabs-content]");
@@ -36,7 +61,7 @@
 })(jQuery);
 
 (function ($) {
-    var checkbox =  $('[data-theme-switcher] input');
+    var checkbox = $('[data-theme-switcher] input');
     checkbox.on('change', function () {
         if (checkbox[0].checked) {
             $(document.body).removeClass('light-theme');
@@ -45,3 +70,48 @@
         }
     });
 })(jQuery);
+
+
+setTimeout(() => {
+    var deviceManagmentTemplate = jQuery('#wallet-item-template').html();
+    var result = '';
+    for (let i = 0; i <= 12; i++) {
+        result += deviceManagmentTemplate
+            .replace(/{device}/g, 'device')
+            .replace(/{location}/g, 'location')
+            .replace(/{activity}/g, 'activity')
+            .replace(/{ip}/g, 'ip')
+    }
+    $('#wallets-list').html(result);
+}, 1000);
+
+
+setTimeout(() => {
+    var deviceManagmentTemplate = jQuery('#device-managment-template').html();
+    var result = '';
+    var container = $('#device-managment-table');
+    for (let i = 0; i <= 20; i++) {
+        result += deviceManagmentTemplate
+            .replace(/{device}/g, 'device')
+            .replace(/{location}/g, 'location')
+            .replace(/{activity}/g, 'activity')
+            .replace(/{ip}/g, 'ip')
+    }
+    container.html(result);
+    new SimpleBar(container[0], {
+        autoHide: false
+    });
+}, 2000);
+
+
+setTimeout(() => {
+    var locationManagmentTemplate = jQuery('#location-managment-template').html();
+    var result = '';
+    for (let i = 0; i <= 20; i++) {
+        result += locationManagmentTemplate
+            .replace(/{location}/g, 'location')
+            .replace(/{activity}/g, 'activity')
+            .replace(/{ip}/g, 'ip')
+    }
+    $('#location-managment-table').html(result);
+}, 3000);
