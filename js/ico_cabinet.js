cd400de0626e21d7e6cf6b1b0fb85527e78c027e
Bohdan Zinkevych <venomsunset@gmail.com>
2019-01-21 18:52:32 +0200

    fix

--- js/ico_cabinet.js
+++ js/ico_cabinet.js
@@ -1,117 +1,581 @@
-;
-(function () {
+var statisticCollection = [{
+    icon: 'img/coins/BTC.svg',
+    name: 'Bitcoin',
+    balance: '9,999.999999999',
+    inOrders: '9,999.999999999',
+    price: '999,999.99 USD',
+    fee: '999,999.99 USD'
+}, {
+    icon: 'img/coins/ETH.svg',
+    name: 'Litecoin',
+    balance: '9,999.999999999',
+    inOrders: '9,999.999999999',
+    price: '999,999.99 USD',
+    fee: '999,999.99 USD'
+}, {
+    icon: 'img/coins/BCH.svg',
+    name: 'Bitcoin Cash',
+    balance: '9,999.999999999',
+    inOrders: '9,999.999999999',
+    price: '999,999.99 USD',
+    fee: '999,999.99 USD'
+}, {
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    balance: '9,999.999999999',
+    inOrders: '9,999.999999999',
+    price: '999,999.99 USD',
+    fee: '999,999.99 USD'
+}, {
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    balance: '9,999.999999999',
+    inOrders: '9,999.999999999',
+    price: '999,999.99 USD',
+    fee: '999,999.99 USD'
+}, {
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    balance: '9,999.999999999',
+    inOrders: '9,999.999999999',
+    price: '999,999.99 USD',
+    fee: '999,999.99 USD'
+}, {
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    balance: '9,999.999999999',
+    inOrders: '9,999.999999999',
+    price: '999,999.99 USD',
+    fee: '999,999.99 USD'
+}, {
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    balance: '9,999.999999999',
+    inOrders: '9,999.999999999',
+    price: '999,999.99 USD',
+    fee: '999,999.99 USD'
+}];
 
-    // var self = this;
-    var clock = $('[data-clock]');
-    var dateEl = clock.find('[data-clock-date]');
-    var time = clock.find('[data-clock-time]');
-    // var self = this;
 
-    var monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ];
-    update();
-    setInterval(function () {
-        update();
-    }, 1000);
-
-    function update() {
-        var date = new Date();
-        dateEl[0].innerHTML = monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
-        time[0].innerHTML = date.toTimeString().split(' ')[0];
+var walletsCollection = [{
+        icon: 'img/coins/BTC.svg',
+        name: 'Bitcoin',
+        abbr: 'BTC',
+        value: '1,100.999999999',
+        usd: 'USD 999,999.99 ',
+        favorite: true
+    },
+    {
+        icon: 'img/coins/BCH.svg',
+        name: 'Bitcoin Cash',
+        abbr: 'BCH',
+        value: '1,100.999999999',
+        usd: 'USD 999,999.99 ',
+        favorite: false
+    },
+    {
+        icon: 'img/coins/ADX.svg',
+        name: 'AdEx',
+        abbr: 'ADX',
+        value: '1,100.999999999',
+        usd: 'USD 999,999.99 ',
+        favorite: false
+    },
+    {
+        icon: 'img/coins/ARDR.svg',
+        name: 'Ardor',
+        abbr: 'ARDR',
+        value: '1,100.999999999',
+        usd: 'USD 999,999.99 ',
+        favorite: false
+    },
+    {
+        icon: 'img/coins/BCN.svg',
+        name: 'Bytecoin',
+        abbr: 'BCN',
+        value: '0.00',
+        usd: 'USD 0.00',
+        favorite: false,
+        inactive: true
+    },
+    {
+        icon: 'img/coins/BCC.svg',
+        name: 'BitConnect',
+        abbr: 'BCC',
+        value: '0.00',
+        usd: 'USD 0.00',
+        favorite: false,
+        inactive: true
+    },
+    {
+        icon: 'img/coins/BAT.svg',
+        name: 'BatCoin',
+        abbr: 'BAT',
+        value: '0.00',
+        usd: 'USD 0.00',
+        favorite: false,
+        inactive: true
+    },
+    {
+        icon: 'img/coins/DGB.svg',
+        name: 'DigiByte',
+        abbr: 'DGB',
+        value: '0.00',
+        usd: 'USD 0.00',
+        favorite: false,
+        inactive: true
+    },
+    {
+        icon: 'img/coins/ETH.svg',
+        name: 'Ethereum',
+        abbr: 'ETH',
+        value: '0.00',
+        usd: 'USD 0.00',
+        favorite: false,
+        inactive: true
+    },
+    {
+        icon: 'img/coins/BNT.svg',
+        name: 'Bancor',
+        abbr: 'BTN',
+        value: '0.00',
+        usd: 'USD 0.00',
+        favorite: false,
+        inactive: true
     }
 
-}());
+];
 
-(function ($) {
-    var tabs = $("[data-tabs-item]");
-    var contents = $("[data-tabs-content]");
 
-    var context = $("[data-tabs]");
-    tabs.each(function (i, tab) {
-        $(tab).on("click", function (e) {
-            tabs.each(function (i, item) {
-                $(item).removeClass("active");
-            });
-            $(tab).addClass("active");
+var transactionsCollection = [{
+    icon: 'img/coins/BTC.svg',
+    name: 'Bitcoin',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+}, {
+    icon: 'img/coins/ETH.svg',
+    name: 'Litecoin',
+    date: '24-01-2018   13:45',
+    status: 'receive',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+}, {
+    icon: 'img/coins/BCH.svg',
+    name: 'Bitcoin Cash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+}, {
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+}, {
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+}, {
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+}, {
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+}, {
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+},
+{
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+},
+{
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+},
+{
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+},
+{
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+},
+{
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+},
+{
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+},
+{
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+},
+{
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+},
+{
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+},
+{
+    icon: 'img/coins/DASH.svg',
+    name: 'Dash',
+    date: '24-01-2018   13:45',
+    status: 'send',
+    amount: '999,999.99',
+    fee: '999,999.99',
+    totalBalance: '999,999.99'
+}
 
-            var id = e.currentTarget.getAttribute("data-tabs-item");
-            var content = context.find('[data-tabs-content="' + id + '"]');
-            contents.each(function (i, item) {
-                $(item).removeClass("active");
-            });
-            content.addClass('active');
-            // if (content) {
-            //   content.addClass("active");
-            //   if (id == 1) {
-            //     bookmark = "orders";
-            //     chart.setBookmark("orders");
-            //   }
-            //   if (id == 2) {
-            //     bookmark = "orders_history";
-            //     chart.setBookmark("orders_history");
-            //   }
-            //   if (id == 3) {
-            //     bookmark = "transactions_history";
-            //     chart.setBookmark("transactions_history");
-            //   }
-            // }
-        });
-    });
-})(jQuery);
+];
 
-(function ($) {
-    var checkbox = $('[data-theme-switcher] input');
-    checkbox.on('change', function () {
-        if (checkbox[0].checked) {
-            $(document.body).removeClass('light-theme');
-        } else {
-            $(document.body).addClass('light-theme');
+
+
+
+Vue.component('app-clock', {
+    data: function() {
+        return {
+            monthNames: ["January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+            ],
+            date: '',
+            time: ''
         }
-    });
-})(jQuery);
+    },
+    template: '#clock-template',
+    mounted: function() {
+        var self = this;
 
-
-setTimeout(() => {
-    var deviceManagmentTemplate = jQuery('#wallet-item-template').html();
-    var result = '';
-    for (let i = 0; i <= 12; i++) {
-        result += deviceManagmentTemplate
-            .replace(/{device}/g, 'device')
-            .replace(/{location}/g, 'location')
-            .replace(/{activity}/g, 'activity')
-            .replace(/{ip}/g, 'ip')
+        this.update();
+        setInterval(function() {
+            self.update();
+        }, 1000);
+    },
+    methods: {
+        update: function() {
+            var date = new Date();
+            this.date = this.monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+            this.time = date.toTimeString().split(' ')[0];
+        }
     }
-    $('#wallets-list').html(result);
-}, 1000);
+});
 
 
-setTimeout(() => {
-    var deviceManagmentTemplate = jQuery('#device-managment-template').html();
-    var result = '';
-    var container = $('#device-managment-table');
-    for (let i = 0; i <= 20; i++) {
-        result += deviceManagmentTemplate
-            .replace(/{device}/g, 'device')
-            .replace(/{location}/g, 'location')
-            .replace(/{activity}/g, 'activity')
-            .replace(/{ip}/g, 'ip')
+Vue.component('app-account', {
+    data: function() {
+        return {
+            valid: false,
+            form: {
+                email: '',
+                password: '',
+                passwordConfirm: ''
+            },
+            errors: {
+                email: false,
+                password: false,
+                passwordConfirm: false
+            },
+            dirty: {
+                email: false,
+                password: false,
+                passwordConfirm: false
+            }
+        }
+    },
+    template: '#account-template',
+    mounted: function() {
+
+    },
+    methods: {
+        submit: function() {
+            if (this.valid) {
+                alert(JSON.stringify(this.form));
+                console.log(JSON.stringify(this.form));
+            }
+        },
+        validate: function() {
+            this.errors.email = !this.validEmail(this.form.email);
+            this.errors.password = !this.validPassword(this.form.password);
+            this.errors.passwordConfirm = this.validPasswordConfirm(this.form.password, this.form.passwordConfirm);
+
+            if (this.errors.email || this.errors.password || this.errors.passwordConfirm) {
+                this.valid = false;
+            } else {
+                this.valid = true;
+            }
+        },
+        validEmail: function(email) {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(email);
+        },
+        validPassword: function(password) {
+            var re = /^([a-zA-Z0-9_]){8,30}$/;
+            return re.test(password);
+        },
+        validPasswordConfirm: function(password, confirm) {
+            return (password === confirm) ? false : true;
+        }
     }
-    container.html(result);
-    new SimpleBar(container[0], {
-        autoHide: false
-    });
-}, 2000);
+});
 
+Vue.component('app-security', {
+    data: function() {
+        return {
+            deviceCollection: [],
+            locationCollection: [],
+            changed: false,
+            cached: null,
+            form: {
+                smsAuth: false,
+                googleAuth: false
+            }
+        }
+    },
+    template: '#security-template',
+    mounted: function() {
+        var self = this;
+        this.cached = JSON.stringify(this.form);
+        setTimeout(function() {
+            for (var i = 0; i <= 20; i++) {
+                self.deviceCollection.push({
+                    device: 'device',
+                    location: 'location',
+                    activity: 'activity',
+                    ip: 'ip'
+                });
+                self.locationCollection.push({
+                    location: 'location',
+                    activity: 'activity',
+                    ip: 'ip'
+                });
+            }
 
-setTimeout(() => {
-    var locationManagmentTemplate = jQuery('#location-managment-template').html();
-    var result = '';
-    for (let i = 0; i <= 20; i++) {
-        result += locationManagmentTemplate
-            .replace(/{location}/g, 'location')
-            .replace(/{activity}/g, 'activity')
-            .replace(/{ip}/g, 'ip')
+            setTimeout(function() {
+                new SimpleBar(self.$refs['device'], {
+                    autoHide: false
+                });
+                new SimpleBar(self.$refs['location'], {
+                    autoHide: false
+                });
+            }, 0);
+        }, 1000);
+    },
+    methods: {
+        submit: function() {
+            alert(JSON.stringify(this.form));
+            this.changed = false;
+            this.cached = JSON.stringify(this.form);
+        },
+
+        validate: function() {
+            if (this.cached === JSON.stringify(this.form)) {
+                this.changed = false;
+            } else {
+                this.changed = true;
+            }
+        }
     }
-    $('#location-managment-table').html(result);
-}, 3000);
+});
+
+
+Vue.component('app-wallets', {
+    data: function() {
+        return {
+            tabs: ['Favorite wallets', 'All wallets', 'Transactions'],
+            activeTab: 0,
+            all: walletsCollection,
+            // favorite: [],
+            transactions: transactionsCollection
+        }
+    },
+    template: '#wallets-template',
+    mounted: function() {
+        var self = this;
+        setTimeout(function() {
+
+            new SimpleBar(self.$refs['favorite'], {
+                autoHide: false
+            });
+
+        }, 0);
+    },
+    computed: {
+        favorite() {
+            return this.all.filter(item => {
+                return item.favorite;
+            })
+        }
+    },
+    methods: {
+        add: function() {
+            alert('add');
+        },
+        send: function() {
+            alert('send');
+        },
+        receive: function() {
+            alert('receive');
+        },
+        showInfo: function() {
+
+        },
+        changeTab: function(i) {
+            var self = this;
+            this.activeTab = i;
+
+            setTimeout(function() {
+                if (i === 0) {
+                    new SimpleBar(self.$refs['favorite'], {
+                        autoHide: false
+                    });
+                }
+                if (i === 1) {
+                    new SimpleBar(self.$refs['all'], {
+                        autoHide: false
+                    });
+                }
+                if (i === 2) {
+                    new SimpleBar(self.$refs['transactions'], {
+                        autoHide: false
+                    });
+                }
+            }, 0);
+        },
+        addToFavorite: function(item) {
+            item.favorite = !item.favorite;
+        }
+    }
+});
+
+
+Vue.component('app-statistic', {
+    data: function() {
+        return {
+            collection: []
+        }
+    },
+    template: '#statistic-info-template',
+    mounted: function() {
+        var self = this;
+        setTimeout(function() {
+            self.collection = statisticCollection;
+            setTimeout(function() {
+                new SimpleBar(self.$refs['scroll'], {
+                    autoHide: false
+                });
+            }, 0);
+        }, 1000);
+    },
+    methods: {
+
+    }
+});
+Vue.component('app-info-tooltip', {
+    data: function() {
+        return {
+
+        }
+    },
+    template: '#info-tooltip-template',
+    mounted: function() {
+
+        var checkbox = $('[data-theme-switcher] input');
+        checkbox.on('change', function() {
+            if (checkbox[0].checked) {
+                $(document.body).removeClass('light-theme');
+            } else {
+                $(document.body).addClass('light-theme');
+            }
+        });
+
+    },
+    methods: {
+
+    }
+});
+
+
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+
+    }
+});
